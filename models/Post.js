98ad2f82ce1ca28cd3a02f42ba1e8adbad55ca44
sagarrao <RaoSa@3901C502874L.corp.espn.pvt>
2015-11-01 20:50:01 +0530

    Adding post processing logic for sending email

--- models/Post.js
+++ models/Post.js
@@ -13,19 +13,39 @@ var Post = new keystone.List('Post', {
 
 Post.add({
 	title: { type: String, required: true },
-	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
-	author: { type: Types.Relationship, ref: 'Genius', index: true },
+	skill: { type: Types.Select, options: 'inter, intra, all', default: 'all', index: true },
+	//author: { type: Types.Relationship, ref: 'Genius', index: true },
 	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
-	image: { type: Types.CloudinaryImage },
+	//image: { type: Types.CloudinaryImage },
 	content: {
-		brief: { type: Types.Html, wysiwyg: true, height: 150 },
-		extended: { type: Types.Html, wysiwyg: true, height: 400 }
+		brief: { type: Types.Html, wysiwyg: true, height: 150 }
 	},
-	categories: { type: Types.Relationship, ref: 'PostCategory', many: true }
+	//categories: { type: Types.Relationship, ref: 'PostCategory', many: true }
 });
 
 Post.schema.virtual('content.full').get(function() {
-	return this.content.extended || this.content.brief;
+	return this.content.brief;
+});
+
+Post.schema.post('save',function(){
+	console.log("Inside save.."+this.skill);
+	keystone.list('Genius').model.find().where('skills', this.skill).exec(function(err, usersBasedOnSkills) {
+		
+		if (err) return callback(err);
+		
+		console.log("usersBasedOnSkills--->"+usersBasedOnSkills);
+		
+		/*new keystone.Email('enquiry-notification').send({
+			to: usersBasedOnSkills,
+			from: {
+				name: 'Find Your Talents Adminn',
+				email: 'contact@find-your-talents-adminn.com'
+			},
+			subject: 'New Enquiry for Find Your Talents Adminn',
+			enquiry: enquiry
+		}, callback);*/
+		
+	});
 });
 
 Post.defaultColumns = 'title, state|20%, author|20%, publishedDate|20%';
